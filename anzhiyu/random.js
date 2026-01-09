c77fc41f5038c02cdca57f616f3785512b72a00d
krez82 <17719918978@163.com>
2026-01-09 11:10:28 +0800

    Site updated: 2026-01-09 11:10:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/fd28.html","posts/2700.html","posts/3290.html","posts/224d.html","posts/d153.html","posts/df2a.html","posts/ab5b.html","posts/fb4c.html","posts/94a6.html"];function toRandomPost(){
+var posts=["posts/fd28.html","posts/2700.html","posts/224d.html","posts/3290.html","posts/df2a.html","posts/ab5b.html","posts/d153.html","posts/fb4c.html","posts/94a6.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
